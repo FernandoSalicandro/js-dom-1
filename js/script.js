36d9820f0bc0428402046b28bd596df09a54eb31
FernandoSalicandro <studio.salicandro@gmail.com>
2025-04-28 23:16:00 +0200

    feat : aggiunta versione compatta con uso di toggle e primo uso di operatore ternario + aggiunta versione bonus - lampadina che lampeggia

--- js/script.js
+++ js/script.js
@@ -6,15 +6,18 @@ console.log(lampadinaImg);
 const btnAccensioneLampadina = document.querySelector(".accendi-btn")
 console.log(btnAccensioneLampadina)
 
+//imposto la variabile flag di stato
 let accesa = false;
 
 btnAccensioneLampadina.addEventListener("click", function () {
-    
+
+    //imposto la callback function in base allo stato del bottone
+
     if (!accesa) {
         btnAccensioneLampadina.classList.add("accendi-btn-pink")
         btnAccensioneLampadina.innerText = "Spegni La Lampadina";
         lampadinaImg.src = "./img/yellow_lamp 1.png"
-        
+        console.log(btnAccensioneLampadina)
         accesa = true;
     }
 
@@ -25,6 +28,74 @@ btnAccensioneLampadina.addEventListener("click", function () {
         accesa = false;
     }
 
-console.log(btnAccensioneLampadina)
+
 })
+
+
+// metodo più compatto tramite toggle
+
+// imposto lo stato
+// let accesa = false;
+// //aggiungo il listener per il click
+// btnAccensioneLampadina.addEventListener("click", function (){
+// //quando avviene il click lo stato cambia da acceso a non acceso
+//     accesa = !accesa
+//     // cambio il testo e la lampadina in base allo stato acceso/non acceso
+//     btnAccensioneLampadina.innerText = accesa ?  "Spegni la Lampadina" : "Accendi La Lampadina";
+//     lampadinaImg.src = accesa ? "./img/yellow_lamp 1.png" : "./img/white_lamp 1.png"
+
+//     // uso la toggle function per aggiungere e rimuovere la classe di stile del bottone
+//     btnAccensioneLampadina.classList.toggle("accendi-btn-pink");
+
+// })
+
+
+//Versione bonus - lampadina che lampeggia;
+
+// const bottoneLampadina = document.querySelector(".accendi-btn");
+// console.log(bottoneLampadina);
+
+// const immagineLampadina = document.getElementById("btn-img");
+// console.log(immagineLampadina);
+
+
+// //setto indicatori di stato
+// let èAccesa = false;
+// let staLampeggiando = false;
+// let interval;
+
+
+// bottoneLampadina.addEventListener("click", function () {
+//     if (!staLampeggiando) {
+//         // Primo click: avvia il lampeggio
+//         bottoneLampadina.classList.add("accendi-btn-pink");
+//         bottoneLampadina.innerText = "Spegni Lampadina";
+
+//         interval = setInterval(function () {
+//             if (èAccesa) {
+//                 immagineLampadina.src = "./img/white_lamp 1.png";
+//                 èAccesa = false;
+//             } else {
+//                 immagineLampadina.src = "./img/yellow_lamp 1.png";
+//                 èAccesa = true;
+//             }
+//         }, 300);
+
+//         staLampeggiando = true;
+
+//     } else {
+//         // Secondo click: ferma il lampeggio
+//         clearInterval(interval);
+//         bottoneLampadina.classList.remove("accendi-btn-pink");
+//         bottoneLampadina.innerText = "Accendi Lampadina";
+//         immagineLampadina.src = "./img/white_lamp 1.png"; // rimetti immagine spenta
+//         èAccesa = false;
+//         staLampeggiando = false;
+//     }
+// });
+
+
+
+
+
 
